163e50177600c512bc5e4c6fd19ed824552e4e19
Yu-Ying Lee <yu-yinglee@Yu-Yings-MacBook-Pro.local>
2020-04-03 09:53:30 -0400

    initial commit

---
+++ src/components/users/Users.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
+
+const Users = ({users, loading}) => {
+        if (loading) {
+            return <Spinner />
+        } else {
+            return (
+                <div className="grid-3">
+                    {users.map(user => 
+                        <UserItem key={user.id} user={user}></UserItem>
+                    )}
+                </div>
+            )
+        }
+    
+        
+    
+}
+
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+}
+
+const userStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    // gridTemplateColums: 'repeat(auto-fit, minmax(30%, 1fr))',
+    gridGap: '1rem'
+}
+
+export default Users
